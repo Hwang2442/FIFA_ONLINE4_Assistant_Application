9fbade77a9c4497f91c864e02114578ca722a420
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2021-12-27 23:54:58 +0900

    Add APIList.cs

---
+++ Unity_fifa4_api/Assets/FIFA4/Scripts/APIList.cs
@@ -0,0 +1,27 @@
+﻿
+/// <summary>
+/// <see href="https://developers.nexon.com/fifaonline4/apiList"/>
+/// </summary>
+public static class APIList
+{
+    private static readonly string rootDefaultUrl = "https://api.nexon.co.kr";
+    private static readonly string rootStaticUrl = "https://static.api.nexon.co.kr";
+
+    #region User information
+
+
+
+    #endregion
+
+    #region Match information
+
+    #endregion
+
+    #region Ranker information
+
+    #endregion
+
+    #region Meta information
+
+    #endregion
+}

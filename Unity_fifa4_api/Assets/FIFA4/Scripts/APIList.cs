e42847730ba8f3c970b14a05357b1eda3f7eb603
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2021-12-31 18:46:27 +0900

    Update APIList.cs

--- Unity_fifa4_api/Assets/FIFA4/Scripts/APIList.cs
+++ Unity_fifa4_api/Assets/FIFA4/Scripts/APIList.cs
@@ -1,15 +1,45 @@
-﻿
+﻿using System.IO;
+
 /// <summary>
 /// <see href="https://developers.nexon.com/fifaonline4/apiList"/>
 /// </summary>
 public static class APIList
 {
-    private static readonly string rootDefaultUrl = "https://api.nexon.co.kr";
-    private static readonly string rootStaticUrl = "https://static.api.nexon.co.kr";
+    private static readonly string rootUrl = "https://api.nexon.co.kr";
+    private static readonly string staticUrl = "https://static.api.nexon.co.kr";
 
     #region User information
 
+    /// <summary>
+    /// <see href="https://developers.nexon.com/fifaonline4/api/6/15"/>
+    /// <para>
+    /// Inquire the user's unique identifier with the user's nickname.
+    ///     <br>
+    ///     User-specific identifiers are used when inquiring user information in other APIs.
+    ///     </br>
+    /// </para>
+    /// </summary>
+    /// <param name="nickname"></param>
+    /// <returns>https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={nickname}</returns>
+    public static string GetUserInformationFromNickname(string nickname)
+    {
+        string url = Path.Combine(rootUrl, "fifaonline4/v1.0/users?nickname={0}");
 
+        return string.Format(url, nickname);
+    }
+
+    /// <summary>
+    /// <see href="https://developers.nexon.com/fifaonline4/api/6/16"/>
+    /// <para>Inquire the user's information with a user-specific identifier {accessid}.</para>
+    /// </summary>
+    /// <param name="accessid"></param>
+    /// <returns>https://api.nexon.co.kr/fifaonline4/v1.0/users/{accessid}</returns>
+    public static string GetUserInformationFromAccessid(string accessid)
+    {
+        string url = Path.Combine(rootUrl, "fifaonline4/v1.0/users/{0}");
+
+        return string.Format(url, accessid);
+    }
 
     #endregion
 
